4d51a11d754669ccea2c1e26a48e04971496ad78
Edgar Reynaga <edgar.reynaga@loop.colum.edu>
2019-02-25 13:30:10 -0600

    Final Edits

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+    {
+        name: 'Leanne',
+        photo: 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        scores: [
+            '1',
+            '5',
+            '5',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Paul',
+        photo: 'https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        scores: [
+            '2',
+            '4',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '2',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Haleema',
+        photo: 'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        scores: [
+            '4',
+            '3',
+            '5',
+            '1',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1'
+        ]
+    },
+    {
+        name: 'Derek',
+        photo: 'https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        scores: [
+            '5',
+            '1',
+            '2',
+            '1',
+            '3',
+            '4',
+            '1',
+            '3',
+            '3',
+            '4'
+        ]
+    },
+    {
+        name: 'Chloe',
+        photo: 'https://images.pexels.com/photos/712521/pexels-photo-712521.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        scores: [
+            '1',
+            '1',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '1'
+        ]
+    },
+    {
+        name: 'Xavier',
+        photo: 'https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        scores: [
+            '1',
+            '1',
+            '5',
+            '1',
+            '3',
+            '2',
+            '5',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Amida',
+        photo: 'https://images.pexels.com/photos/371160/pexels-photo-371160.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        scores: [
+            '5',
+            '4',
+            '4',
+            '2',
+            '4',
+            '4',
+            '3',
+            '2',
+            '3',
+            '3'
+        ]
+    },
+    {
+        name: 'Saahirah',
+        photo: 'https://images.pexels.com/photos/769024/pexels-photo-769024.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        scores: [
+            '5',
+            '2',
+            '3',
+            '1',
+            '5',
+            '1',
+            '3',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Omar',
+        photo: 'https://images.pexels.com/photos/793253/pexels-photo-793253.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        scores: [
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5'
+        ]
+    },
+    {
+        name: 'Aliyah',
+        photo: 'https://images.pexels.com/photos/1181519/pexels-photo-1181519.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        scores: [
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1'
+        ]
+    }
+];
+
+module.exports = friendsArray;
